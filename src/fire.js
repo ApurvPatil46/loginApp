ad51b09905f444a6c0717701e3e2e71a2911a75f
Apurv Nilkanth Patil <52930645+ApurvPatil46@users.noreply.github.com>
2021-01-22 16:06:27 +0530

    Login Application Using Firebase
    Login Application Using Firebase

---
+++ src/fire.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase';
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyDAP1bJPxz1rEe8WDMiZ5L97F00cxkkuvs",
+    authDomain: "login-4b024.firebaseapp.com",
+    projectId: "login-4b024",
+    storageBucket: "login-4b024.appspot.com",
+    messagingSenderId: "1035973520445",
+    appId: "1:1035973520445:web:a8ca2501bc73a7cc2d8e54"
+};
+  // Initialize Firebase
+const fire = firebase.initializeApp(firebaseConfig);
+
+export default fire ; 
