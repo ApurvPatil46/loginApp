cf5a2865c1d472907b3b2b839bf5ed130ef457d3
Apurv Nilkanth Patil <52930645+ApurvPatil46@users.noreply.github.com>
2021-03-01 16:59:45 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,103 +1,38 @@
-import React , {useState , useEffect} from 'react'
-import fire from './fire'
-import Login from './login'
-import Hero from './hero'
-import './App.css'
+import React,{useContext, useState} from "react";
+import {NameOfPikachu , MovesOfPikachu} from "./Context" 
 
 const App = () => {
-    const [user , setUser] = useState('');
-    const [email , setEmail] = useState('');
-    const [password , setPassword] = useState('');
-    const [emailError , setEmailError] = useState('');
-    const [passwordError , setPasswordError] = useState('');
-    const [hasAccount , setHasAccount] = useState(false);
+    const [num , setNum] = useState();
 
-    const clearInputs = () => {
-        setEmail('');
-        setPassword('');
-    }
-
-    const clearErrors = () => {
-        setEmailError('');
-        setPasswordError('');
-    }
-
-    const handleLogin = () => {
-        clearErrors();
-        fire
-            .auth()
-            .signInWithEmailAndPassword(email , password)
-            .catch(err=>{
-                switch(err.code){
-                    case "auth/invalid-email":
-                    case "auth/user-disabled":
-                    case "auth/user-not-found":
-                        setEmailError(err.message);
-                        break;
-                    case "auth/wrong-password":
-                        setPasswordError(err.message);
-                        break;
-                }
-            })
-    }    
+    const Name = useContext(NameOfPikachu);
+    const Moves = useContext(MovesOfPikachu);
     
-    const handleSignup = () => {
-        clearErrors();
-        fire
-            .auth()
-            .createUserWithEmailAndPassword(email , password)
-            .catch(err => {
-                switch(err.code){
-                    case "auth/email-already-in-use":
-                    case "auth/invalid-email":
-                        setEmailError(err.message);
-                        break;
-                    case "auth/weak-password":
-                        setPasswordError(err.message);
-                        break;
-                }
-            })
-    }
-    
-    const handleLogout = () => {
-        fire.auth().signOut();
-    }
-
-    const authListener = () => {
-        fire.auth().onAuthStateChanged(user =>{
-            if(user){
-                clearInputs();
-                setUser(user);
-            }else{
-                setUser("");
-            }
-        })
-    }
-
-    useEffect(()=>{
-        authListener();
-    },[])
-
+    let NumberClicled = {num}
     return(
-            <div className = "App">
-            <h1> Heading Tag Added</h1>
-                {user ?(<Hero handleLogout = {handleLogout}/>):
-                (
-                    <Login 
-                        email = {email} 
-                        setEmail = {setEmail} 
-                        password = {password} 
-                        setPassword = {setPassword}
-                        handleLogin = {handleLogin}
-                        handleSignup = {handleSignup}
-                        hasAccount = {hasAccount}
-                        setHasAccount = {setHasAccount}
-                        emailError = {emailError}
-                        passwordError = {passwordError}
-                    />
-                )}
+        <>
+            <div style = {{
+                position : "fixed",
+                left : "50%",
+                top: "50%",
+                marginTop : "-50px",
+                marginLeft : "-100px"}}>
+                
+                <h1>Pokemon</h1>
+                
+                <select value = {num} onChange = {(event) => {
+                    setNum(event.target.value)
+                }}>
+                    <option value = "1">1</option>
+                    <option value = "2">2</option>
+                    <option value = "3">3</option>
+                    <option value = "25">25</option>
+                </select>
+                <h2>You Have Choose {NumberClicled}</h2>
+                <h3>The Name of Pokemon is {Name} </h3>
+                <h3>The Number of Moves are {Moves}</h3>
             </div>
-        )
+        </>
+    );
 }
 
 export default App;
